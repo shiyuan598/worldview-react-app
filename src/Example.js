a9c1e005f4ce8725dee9b6546db894544baf6856
王士远 <wangshiyuan@192.168.2.104>
2023-01-14 17:55:29 +0800

    add model

--- src/Example.js
+++ src/Example.js
@@ -1,6 +1,11 @@
 import React from "react";
-
-import Worldview, { Cubes, Axes } from "@foxglove/regl-worldview";
+import Worldview, {
+  Cubes,
+  Axes,
+  Text,
+  GLTFScene
+} from "@foxglove/regl-worldview";
+import duckModel from "./duck.glb";
 
 function BasicExample() {
   const markers = [
@@ -9,7 +14,7 @@ function BasicExample() {
         orientation: { x: 0, y: 0, z: 0, w: 1 },
         position: { x: 0, y: 0, z: 0 }
       },
-      scale: { x: 15, y: 15, z: 15 },
+      scale: { x: 1, y: 1, z: 1 },
       color: { r: 1, g: 0, b: 1, a: 0.9 }
     }
   ];
@@ -18,6 +23,28 @@ function BasicExample() {
     <Worldview>
       <Cubes>{markers}</Cubes>
       <Axes />
+      {/* <Text autoBackgroundColor>
+        {[
+          {
+            text: "Hello, WorldView!",
+            pose: {
+              orientation: { x: 0, y: 0, z: 0, w: 1 },
+              position: { x: 0, y: 5, z: 10 }
+            },
+            scale: { x: 1, y: 1, z: 1 },
+            color: { r: 0, g: 1, b: 1, a: 0.9 }
+          }
+        ]}
+      </Text> */}
+      <GLTFScene model={duckModel}>
+        {{
+          pose: {
+            position: { x: 0, y: 0, z: 0 },
+            orientation: { x: 0, y: 0, z: 0, w: 1 }
+          },
+          scale: { x: 8, y: 8, z: 8 }
+        }}
+      </GLTFScene>
     </Worldview>
   );
 }
